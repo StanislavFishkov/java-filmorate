f79c30aa607fc4df8c3bb89adb96e9620b393f60
Stanislav Fishkov <fishkovs@yandex.ru>
2024-06-20 23:46:04 +0200

    Make implementation of sprint 10

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,73 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private Map<Integer, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> getAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@Valid @RequestBody User user) {
+        checkUserConstraints(user);
+
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+        log.info("User is created: {}", user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@Valid @RequestBody User newUser) {
+        if (newUser.getId() == null) {
+            throw new ValidationException("User id can't be null on update: " + newUser, log);
+        }
+
+        if (users.containsKey(newUser.getId())) {
+            checkUserConstraints(newUser);
+
+            users.put(newUser.getId(), newUser);
+            log.info("User is updated: {}", newUser);
+            return newUser;
+        }
+        throw new ValidationException("User can't be found by id: " + newUser, log);
+    }
+
+    private void checkUserConstraints(User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            throw new ValidationException("User email can't be empty and should contain @: " + user, log);
+        }
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("User login can't be empty or contain spaces: " + user, log);
+        }
+        if (user.getBirthday() == null || user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("User birthday can't be empty or in the future: " + user, log);
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+    }
+
+    private int getNextId() {
+        return users.keySet()
+                .stream()
+                .mapToInt(Integer::intValue)
+                .max()
+                .orElse(0) + 1;
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -0,0 +1,73 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+    UserController userController;
+    User verifiedUser;
+
+    @BeforeEach
+    void setUp() {
+        userController = new UserController();
+        verifiedUser = User.builder()
+                .id(1)
+                .email("email@domen.com")
+                .login("Login")
+                .birthday(LocalDate.of(1984, 12, 26))
+                .name("Name")
+                .build();
+    }
+
+    @Test
+    void createUser() {
+        userController.create(verifiedUser);
+        assertEquals(1, userController.getAll().size());
+        assertEquals(verifiedUser.getName(), userController.getAll().stream().findFirst().get().getName());
+    }
+
+    @Test
+    void updateFilm() {
+        userController.create(verifiedUser);
+        userController.update(verifiedUser.toBuilder().login("Different").build());
+
+        assertEquals(1, userController.getAll().size());
+        assertNotEquals(verifiedUser.getLogin(), userController.getAll().stream().findFirst().get().getLogin());
+    }
+
+    @Test
+    void emailNotEmptyContainsAt() {
+        User userBlankEmail = verifiedUser.toBuilder().email(" ").build();
+        assertThrows(ValidationException.class, () -> userController.create(userBlankEmail));
+
+        User user = verifiedUser.toBuilder().email("email$domain.com").build();
+        assertThrows(ValidationException.class, () -> userController.create(user));
+    }
+
+    @Test
+    void loginNotEmptyWithoutSpaces() {
+        User user = verifiedUser.toBuilder().login("test test").build();
+        assertThrows(ValidationException.class, () -> userController.create(user));
+    }
+
+    @Test
+    void nameCanBeEmpty() {
+        User user = verifiedUser.toBuilder().name(null).build();
+        assertDoesNotThrow(() -> userController.create(user));
+
+        User userFromController = userController.getAll().stream().findFirst().get();
+        assertEquals(verifiedUser.getLogin(), userFromController.getName());
+    }
+
+    @Test
+    void birthdayNotInTheFuture() {
+        User user = verifiedUser.toBuilder().birthday(LocalDate.of(LocalDate.now().getYear() + 1, 1, 1)).build();
+        assertThrows(ValidationException.class, () -> userController.create(user));
+    }
+}
